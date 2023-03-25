d827353456ef6004bc3ae990573d6918a8fac92c
snowdonstudio <63182046+snowdonstudio@users.noreply.github.com>
2023-03-25 23:09:08 +0530

    Stage this

--- js/index.js
+++ js/index.js
@@ -1,5 +1,152 @@
   //page url = http://127.0.0.1:5500/index.html?p1=11.401736541896099&p2=76.683730423760906
 
+
+  //dijkstra's algorithm
+  var Graph = (function (undefined) {
+
+	var extractKeys = function (obj) {
+		var keys = [], key;
+		for (key in obj) {
+		    Object.prototype.hasOwnProperty.call(obj,key) && keys.push(key);
+		}
+		return keys;
+	}
+
+	var sorter = function (a, b) {
+		return parseFloat (a) - parseFloat (b);
+	}
+
+	var findPaths = function (map, start, end, infinity) {
+		infinity = infinity || Infinity;
+
+		var costs = {},
+		    open = {'0': [start]},
+		    predecessors = {},
+		    keys;
+
+		var addToOpen = function (cost, vertex) {
+			var key = "" + cost;
+			if (!open[key]) open[key] = [];
+			open[key].push(vertex);
+		}
+
+		costs[start] = 0;
+
+		while (open) {
+			if(!(keys = extractKeys(open)).length) break;
+
+			keys.sort(sorter);
+
+			var key = keys[0],
+			    bucket = open[key],
+			    node = bucket.shift(),
+			    currentCost = parseFloat(key),
+			    adjacentNodes = map[node] || {};
+
+			if (!bucket.length) delete open[key];
+
+			for (var vertex in adjacentNodes) {
+			    if (Object.prototype.hasOwnProperty.call(adjacentNodes, vertex)) {
+					var cost = adjacentNodes[vertex],
+					    totalCost = cost + currentCost,
+					    vertexCost = costs[vertex];
+
+					if ((vertexCost === undefined) || (vertexCost > totalCost)) {
+						costs[vertex] = totalCost;
+						addToOpen(totalCost, vertex);
+						predecessors[vertex] = node;
+					}
+				}
+			}
+		}
+
+		if (costs[end] === undefined) {
+			return null;
+		} else {
+			return predecessors;
+		}
+
+	}
+
+	var extractShortest = function (predecessors, end) {
+		var nodes = [],
+		    u = end;
+
+		while (u !== undefined) {
+			nodes.push(u);
+			u = predecessors[u];
+		}
+
+		nodes.reverse();
+		return nodes;
+	}
+
+	var findShortestPath = function (map, nodes) {
+		var start = nodes.shift(),
+		    end,
+		    predecessors,
+		    path = [],
+		    shortest;
+
+		while (nodes.length) {
+			end = nodes.shift();
+			predecessors = findPaths(map, start, end);
+
+			if (predecessors) {
+				shortest = extractShortest(predecessors, end);
+				if (nodes.length) {
+					path.push.apply(path, shortest.slice(0, -1));
+				} else {
+					return path.concat(shortest);
+				}
+			} else {
+				return null;
+			}
+
+			start = end;
+		}
+	}
+
+	var toArray = function (list, offset) {
+		try {
+			return Array.prototype.slice.call(list, offset);
+		} catch (e) {
+			var a = [];
+			for (var i = offset || 0, l = list.length; i < l; ++i) {
+				a.push(list[i]);
+			}
+			return a;
+		}
+	}
+
+	var Graph = function (map) {
+		this.map = map;
+	}
+
+	Graph.prototype.findShortestPath = function (start, end) {
+		if (Object.prototype.toString.call(start) === '[object Array]') {
+			return findShortestPath(this.map, start);
+		} else if (arguments.length === 2) {
+			return findShortestPath(this.map, [start, end]);
+		} else {
+			return findShortestPath(this.map, toArray(arguments));
+		}
+	}
+
+	Graph.findShortestPath = function (map, start, end) {
+		if (Object.prototype.toString.call(start) === '[object Array]') {
+			return findShortestPath(map, start);
+		} else if (arguments.length === 3) {
+			return findShortestPath(map, [start, end]);
+		} else {
+			return findShortestPath(map, toArray(arguments, 1));
+		}
+	}
+
+	return Graph;
+
+})();
+
   const infoPanelClose = document.getElementById('close-info');
   const contentBox = document.getElementById('my-content');
   const contenBoxInfo = document.getElementById('content-info');
@@ -39,6 +186,86 @@
 
   // Creating a map object
   var map = new L.map('map', mapOptions);
+  
+  var pts = {4:{3:16.1,2:28.3,1:38.1,6:35.3,7:27.6,10:20.6},10:{11:5.1,12:8.1,13:14.4,6:8.9,4:16.4,8:24.2},9:{8:7.6,3:14.5,2:20.2,1:25.9,6:21.0,13:18.5}}
+  var m = {1:{2:11.8,3:22.1},2:{1:11.8,3:25.5},3:{1:22.1,2:25.5}}
+  var alg = {a:{b:3,c:1},b:{a:2,c:1},c:{a:4,b:1}}
+  
+  graph = new Graph(pts);
+  // var path = graph.findShortestPath(4,10);
+  console.log(graph.findShortestPath('4','10'));
+
+  //Extending the dijkstra's algorithm
+
+  var curr_node=0;
+  map.on('click', function (e) {
+   var node = [];
+   curr_node = curr_node+1;
+   node[0] = e.latlng.lat;
+   node[1] = e.latlng.lng;
+   addNodes(node,curr_node);
+
+   console.log(node);
+   console.log(curr_node);
+});
+
+var textarea_nodes = document.getElementById('textarea_nodes')
+function addNodes(polyNodes,num){
+   var marker = L.marker([polyNodes[0], polyNodes[1]]).addTo(map);
+   var myIcon = L.icon({
+       iconUrl: 'assets/icons/building.png',
+       iconSize: [32, 32],
+       iconAnchor: [16, 16],
+       popupAnchor: [-3, -76]
+   });
+   marker.setIcon(myIcon);
+   marker.on('click',(event) => {
+       if(window.event.ctrlKey){
+        console.log('control click');
+           curr_ctrl_pt = [polyNodes[0], polyNodes[1]];
+           var oldText = $('#textarea_nodes').val();
+          //  var oldText = textarea_nodes.innerText;
+           console.log('oldText : '+oldText)
+          //  textarea_nodes.innerText = oldText + num + ':{';
+           $('#textarea_nodes').val(oldText + num + ':{');
+           var myIcon_2 = L.icon({
+               iconUrl: 'assets/icons/hotspot.png',
+               iconSize: [32, 32],
+               iconAnchor: [16, 16],
+               popupAnchor: [-3, -76]
+           });
+           marker.setIcon(myIcon_2); // custom marker to show that the node has been declared. This is just for your own reference
+       }
+       if(window.event.altKey){
+        console.log('alt click');
+           var oldText = $('#textarea_nodes').val();
+        //  var oldText = textarea_nodes.innerText;
+        //  textarea_nodes.innerText = oldText + num + ':' + getDistanceFromLatLonInM(polyNodes[0], polyNodes[1],curr_ctrl_pt[0],curr_ctrl_pt[1]) + ',';
+         console.log('oldText : '+oldText)
+           $('#textarea_nodes').val(oldText + num + ':' + getDistanceFromLatLonInM(polyNodes[0], polyNodes[1],curr_ctrl_pt[0],curr_ctrl_pt[1]) + ',');
+           }
+       })
+   }
+   function getDistanceFromLatLonInM(lat1,lon1,lat2,lon2) {
+     var R = 6371; // Radius of the earth in km
+     var dLat = deg2rad(lat2-lat1);  // deg2rad below
+     var dLon = deg2rad(lon2-lon1); 
+     var a = 
+       Math.sin(dLat/2) * Math.sin(dLat/2) +
+       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+       Math.sin(dLon/2) * Math.sin(dLon/2)
+       ; 
+     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+     var d = R * c; // Distance in km
+     return (d * 1000).toFixed(1);
+   }
+   
+   function deg2rad(deg) {
+     return deg * (Math.PI/180)
+   }
+
+   var g = Graph(map);
+   console.log('g : '+g);
 
   // Creating a Layer object
   // var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
